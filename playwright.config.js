48ee8878d5812510b7be68bc85d7b8702e11e2f8
Steven Boutcher <boutchersj@gmail.com>
2024-10-11 23:53:38 -0500

    Add list reporter

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['list']
+  ],
   use: {
     headless: false,
     trace: 'on-first-retry',
