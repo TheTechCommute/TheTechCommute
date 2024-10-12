09c2b853501d914736ff2eb4e4acfbee926cc97c
Steven Boutcher <boutchersj@gmail.com>
2024-10-11 23:51:39 -0500

    Add first Playwright test to the project

---
+++ playwright.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    headless: false,
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npx http-server . -p 8080',
+    port: 8080,
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
